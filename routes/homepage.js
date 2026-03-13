ed3be9649b45244b322f814b6372e36ab2acc6fc
scifi <leith.huddleston@gmail.com>
2026-03-12 20:30:48 -0600

    added enrollment and disenrollment routes and fixed class model

--- routes/homepage.js
+++ routes/homepage.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Topic = require('../models/Topic');
 const Class = require('../models/Class');
+const verifyToken = require('../middleware/verify-token');
 
 // GET /home - Fetch all data needed for the public landing page
 
@@ -23,5 +24,58 @@ router.get('/', async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 });
+
+router.put('/:classId/enroll', verifyToken, async (req, res) => {
+    try {
+        const enrolledClass = await Class.findById(req.params.classId)
+        if(!enrolledClass) {
+            res.status(404)
+            throw new Error('Class not found')
+        }
+
+        const studentInClass = enrolledClass.enrollment.findOne({ _id: req.body._id })
+        if(studentInClass) {
+            return res.status(409).json({ error: 'User already enrolled in class'})
+        }
+
+        enrolledClass.enrollment.push(req.body)
+        await enrolledClass.save()
+        res.status(200).json(enrolledClass)
+    } catch (error) {
+        if(res.statusCode === 404){
+            res.json({ error: error.message })
+        } else {
+            res.status(500).json({ error: error.message })
+        }        
+    }
+})
+
+router.put('/:classId/disenroll', verifyToken, async (req, res) => {
+    try {
+        const disenrolledClass = await Class.findById(req.params.classId)
+        if(!disenrolledClass) {
+            res.status(404)
+            throw new Error('Class not found')
+        }
+
+        const indexOfStudent = disenrolledClass.enrollment.findIndex((student) => student._id === req.body._id)
+        if(!indexOfStudent) {
+            res.status(404)
+            throw new Error('Student not found')
+        }
+        
+        disenrolledClass.enrollment.splice(indexOfStudent, 1)
+        await disenrolledClass.save()
+        res.status(200).json(disenrolledClass)
+    } catch (error) {
+        if(res.statusCode === 404){
+            res.json({ error: error.message })
+        } else {
+            res.status(500).json({ error: error.message })
+        }        
+    }
+})
+
+
 
 module.exports = router;
